33467186e74f32e10afc68f8c6f768b6c3974eb9
Black--Snow <calvinsoueid@gmail.com>
2022-04-13 16:57:37 +1000

    Add input module unit tests

--- EngineTests/EngineTests.cpp
+++ EngineTests/EngineTests.cpp
@@ -2,8 +2,19 @@
 #include "CppUnitTest.h"
 
 #include "../Engine/utility/CCXEvent.h"
+#include "../Engine/Modules/Input/Input.h"
+#include "../Engine/Engine.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+namespace Utility
+{
+	inline const std::wstring Concat(wchar_t* str, double val)
+	{
+		return std::wstring(str) + std::to_wstring(val);
+	}
+}
 
 namespace EngineTests
 {
@@ -50,6 +61,118 @@ namespace EngineTests
 			Assert::IsTrue(e.Deregister("Test"));
 			e.Invoke(i);
 			Assert::AreEqual(5, i);
+		}
+	};
+
+	TEST_CLASS(Input)
+	{
+	public:
+		TEST_CLASS_INITIALIZE(Init)
+		{
+			ecse::TestInit();
+		}
+
+		TEST_METHOD_CLEANUP(Cleanup)
+		{
+			ecse::Input::Reset(true);
+		}
+
+		TEST_METHOD(Create_Key)
+		{
+			auto binding = ecse::Input::CreateBinding(nullptr, ecse::Input::Key::A);
+		}
+
+		TEST_METHOD(Create_Mouse)
+		{
+			auto binding = ecse::Input::CreateBinding(nullptr, ecse::Input::Mouse::ButtonLeft);
+		}
+
+		TEST_METHOD(GetData_Key)
+		{
+			auto binding = ecse::Input::CreateBinding(nullptr, ecse::Input::Key::A);
+			binding->master.ForcePoll();
+			auto fData = binding->GetData<float>();
+			Assert::AreEqual(0.0f, fData, 0.01f, L"Unexpected float value");
+		}
+		
+		TEST_METHOD(GetData_MouseButton)
+		{
+			auto binding = ecse::Input::CreateBinding(nullptr, ecse::Input::Mouse::ButtonLeft);
+			binding->master.ForcePoll();
+			auto fData = binding->GetData<float>();
+			Assert::AreEqual(0.0f, fData, 0.01f, L"Unexpected float value");
+		}
+
+		TEST_METHOD(GetData_MousePosition)
+		{
+			auto binding = ecse::Input::CreateBinding(nullptr, ecse::Input::Mouse::Position);
+			binding->master.ForcePoll();
+			double expectedX, expectedY;
+			glfwGetCursorPos(ecse::GetWindows()[0], &expectedX, &expectedY);
+			auto vData = binding->GetData<glm::dvec2>();
+			Assert::IsTrue(vData.x > -25000, Utility::Concat(L"x value below -25000, actual: ", vData.x).c_str());
+			Assert::IsTrue(vData.x < 25000, Utility::Concat(L"x value above 25000, actual: ", vData.x).c_str());
+			Assert::IsTrue(vData.y > -25000, Utility::Concat(L"y Value below -25000, actual: ", vData.y).c_str());
+			Assert::IsTrue(vData.y < 25000, Utility::Concat(L"y value above 25000, actual: ", vData.y).c_str());
+			Assert::AreEqual(expectedX, vData.x);
+			Assert::AreEqual(expectedY, vData.y);
+		}
+
+		TEST_METHOD(Create_Action)
+		{
+			auto action = ecse::Input::CreateAction("test", ecse::Input::Output::Scalar);
+			Assert::IsNotNull(action);
+		}
+
+		TEST_METHOD(Callback_KeyStart)
+		{
+			auto action = ecse::Input::CreateAction("test", ecse::Input::Output::Scalar);
+			auto binding = ecse::Input::CreateBinding(action, ecse::Input::Key::A);
+			bool eventFired = false;
+			action->OnStart.Register([&eventFired](ecse::Input::InputEventData& _) { eventFired = true; });
+			ecse::Input::GLFWKeyCallback(ecse::GetWindows()[0], GLFW_KEY_A, GLFW_PRESS, GLFW_PRESS, 0);
+			Assert::IsTrue(eventFired);
+		}
+
+		TEST_METHOD(Callback_KeyRepeat)
+		{
+			auto action = ecse::Input::CreateAction("test", ecse::Input::Output::Scalar);
+			auto binding = ecse::Input::CreateBinding(action, ecse::Input::Key::A);
+			bool eventFired = false;
+			action->OnRepeat.Register([&eventFired](ecse::Input::InputEventData& _) { eventFired = true; });
+			ecse::Input::GLFWKeyCallback(ecse::GetWindows()[0], GLFW_KEY_A, GLFW_REPEAT, GLFW_REPEAT, 0);
+			Assert::IsTrue(eventFired);
+		}
+		
+		TEST_METHOD(Callback_KeyEnd)
+		{
+			auto action = ecse::Input::CreateAction("test", ecse::Input::Output::Scalar);
+			auto binding = ecse::Input::CreateBinding(action, ecse::Input::Key::A);
+			bool eventFired = false;
+			action->OnEnd.Register([&eventFired](ecse::Input::InputEventData& _) { eventFired = true; });
+			ecse::Input::GLFWKeyCallback(ecse::GetWindows()[0], GLFW_KEY_A, GLFW_RELEASE, GLFW_RELEASE, 0);
+			Assert::IsTrue(eventFired);
+		}
+
+		TEST_METHOD(Context_Set)
+		{
+			auto action = ecse::Input::CreateAction("test", ecse::Input::Output::Scalar);
+			ecse::Input::SetBindContext(action);
+			auto binding = ecse::Input::CreateBinding(ecse::Input::Key::A);
+			bool eventFired = false;
+			action->OnStart.Register([&eventFired](ecse::Input::InputEventData& _) { eventFired = true; });
+			ecse::Input::GLFWKeyCallback(ecse::GetWindows()[0], GLFW_KEY_A, GLFW_PRESS, GLFW_PRESS, 0);
+			Assert::IsTrue(eventFired);
+		}
+
+		TEST_METHOD(Context_Register)
+		{
+			ecse::Input::SetBindContext(ecse::Input::CreateAction("test", ecse::Input::Output::Scalar));
+			auto binding = ecse::Input::CreateBinding(ecse::Input::Key::A);
+			bool eventFired = false;
+			ecse::Input::Context()->OnStart.Register([&eventFired](ecse::Input::InputEventData& _) { eventFired = true; });
+			ecse::Input::GLFWKeyCallback(ecse::GetWindows()[0], GLFW_KEY_A, GLFW_PRESS, GLFW_PRESS, 0);
+			Assert::IsTrue(eventFired);
 		}
 	};
 }
